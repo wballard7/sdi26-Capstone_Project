346a05239f82d1e1365bb01f0b08c24a700737c3
ausray1294 <ausray1294@gmail.com>
2024-08-19 11:41:34 -0600

    updated with pull from main

--- unit-tracker-client/src/components/UserProfile.jsx
+++ unit-tracker-client/src/components/UserProfile.jsx
@@ -1,3 +1,16 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Box, Button, Text, Heading, Link } from '@chakra-ui/react';
+import { UserContext } from '../context/UserContext';
+
 export const UserProfile = () => {
-  return <h1>user profile</h1>;
+  const { user } = useContext(UserContext);
+
+  return (
+    <Box>
+      <h1>user profile</h1>
+      <div>{user.last_name}</div>;
+      <div>{user.first_name}</div>;
+      <div> SELECT unit_name FROM units WHERE id = {user.my_unit_id} </div> // user's unit name
+    </Box>
+  );
 };
