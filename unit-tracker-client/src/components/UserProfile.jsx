867f5bef313f6f53684c7dd56fbf96e31791b4cb
ausray1294 <ausray1294@gmail.com>
2024-08-19 15:49:51 -0600

    updated information to work

--- unit-tracker-client/src/components/UserProfile.jsx
+++ unit-tracker-client/src/components/UserProfile.jsx
@@ -1,11 +1,28 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Box, Heading, Text, Card, CardHeader, CardBody, Button, Link } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  Button,
+  Link,
+  Avatar,
+  VStack,
+  Stack,
+} from '@chakra-ui/react';
 import { UserContext } from '../context/UserContext';
+import { SupervisorContext } from '../context/SupervisorContext';
+import { PersonnelContext } from '../context/PersonnelContext';
 import { getFetch } from '../utils/Fetches';
 
 export const UserProfile = () => {
   const { id, first_name, last_name, org } = useContext(UserContext);
+  const { personnelList } = useContext(PersonnelContext);
   const [availability, setAvailability] = useState(false);
+  const { first_name: supervisor_first_name, last_name: supervisor_last_name } =
+    useContext(SupervisorContext);
 
   useEffect(() => {
     const fetchAvailability = async () => {
@@ -29,7 +46,9 @@ export const UserProfile = () => {
           <Text>First Name: {first_name}</Text>
           <Text>Last Name: {last_name}</Text>
           <Text>Org: {org?.name}</Text>
-          <Text>Supervisor: {/* Add supervisor details here */}</Text>
+          <Text>
+            Supervisor: {supervisor_first_name} {supervisor_last_name}
+          </Text>
           <Box mt={4}>
             {availability ? (
               <Heading size="sm" color="green.500">
@@ -40,6 +59,35 @@ export const UserProfile = () => {
                 Not Available
               </Heading>
             )}
+          </Box>
+          <Box>
+            <VStack align="stretch">
+              <Heading>Personnel</Heading>
+              {personnelList.length > 0 ? (
+                personnelList.map((person) => (
+                  <Stack
+                    key={person.id}
+                    direction="row"
+                    align="center"
+                    p={4}
+                    borderWidth="1px"
+                    borderRadius="md"
+                    spacing={4}
+                    mb={2}
+                  >
+                    <Avatar name={`${person.first_name} ${person.last_name}`} />
+                    <Box>
+                      <Text fontWeight="bold">{`${person.first_name} ${person.last_name}`}</Text>
+                      <Text fontSize="sm" color="gray.500">
+                        {person.role || 'Role not specified'}
+                      </Text>
+                    </Box>
+                  </Stack>
+                ))
+              ) : (
+                <Text>You have no personnel</Text>
+              )}
+            </VStack>
           </Box>
         </CardBody>
       </Card>
